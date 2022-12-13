13bd73c940afada42e0e9e7c79e6e30dff32c59e
Betty9081 <bettykebede1981@gmail.com>
2022-12-13 14:00:37 -0800

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day
- */
+*jack_bouer - prints every minute of the day
+*Return: Always 0
+*/
 
-void jack_bauer(void)
+void jack_bouer(void)
 {
 	int hour, minutes;
+
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
