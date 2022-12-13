0302d97a63b718b3818d5be70b422cc4cd02f79a
Betty9081 <bettykebede1981@gmail.com>
2022-12-13 12:39:51 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - competes the absoulute value of an integer
+*@t: the integer to be competed
+*Return: The absoulte value
+*/
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
