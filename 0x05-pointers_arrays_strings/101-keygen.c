c4c1177ab587a47246bb28fadd621dbe0ec025e1
Betty9081 <bettykebede1981@gmail.com>
2022-12-19 11:59:08 -0800

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates randome valid password
+ * Return: 0 (on Success)
+ */
+
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%", pass);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
