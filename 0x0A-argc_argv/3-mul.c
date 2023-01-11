02502896b3de83c8bbb6e9ea6b450fae8fc43866
Betty9081 <bettykebede1981@gmail.com>
2023-01-11 02:00:12 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -21,6 +21,7 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -34,12 +35,13 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
+			f = 0;
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
 
@@ -62,6 +64,7 @@ int main(int argc, char *argv[])
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
 	return (0);
 }
